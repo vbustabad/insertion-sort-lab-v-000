506164a2a47fe1ab25a701e5b1709643a64e5c34
Veronica Bustabad <vbustabad@gmail.com>
2019-01-14 04:42:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function insertionSort(array){
   let newMin;
   let sorted = [];
   while (array.length != 0) {
-    newMin = minAndRemove(array);
+    newMin = findMinAndRemove(array);
     sorted.push(newMin);
   }
   return sorted;
