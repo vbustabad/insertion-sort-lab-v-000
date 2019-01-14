35519a39e631dd142fc8d63e7cc6eb603aed7e2c
Veronica Bustabad <vbustabad@gmail.com>
2019-01-14 04:41:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function findMinAndRemove(array){
-  let min = array[0]
-  let minIndex = 0
-  for(let i = 0; i < array.length; i++){
-    let currentElement = array[i]
-    if(array[i] < min){
-      min = array[i]
-      minIndex = i
+  let min = array[0];
+  let minIndex = 0;
+  for (let i = 0; i < array.length; i++) {
+    let currentElement = array[i];
+    if (array[i] < min) {
+      min = array[i];
+      minIndex = i;
     }
   }
   array.splice(minIndex, 1);
